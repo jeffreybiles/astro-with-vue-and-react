26abd8d46ed75b4d206149ca828e454c79ebd66c
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-04 16:34:58 -0600

    Add Vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div className="vue-component">
+    <div>
+      {{count}}
+    </div>
+    <button @click="() => count = count + 1">
+      + 1
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0)
+</script>
+
+<style scoped>
+
+</style>
