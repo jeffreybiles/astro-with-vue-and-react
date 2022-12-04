8ad6b6ef5ae8efbecd538591ff8b9527621b8dcf
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-04 17:20:07 -0600

    Using React inside Vue

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,20 +1,28 @@
 <template>
   <div className="vue-component">
+    <Hello />
     <div>
       {{count}}
     </div>
     <button @click="() => count = count + 1">
       + 1
     </button>
+    <div style="border:1px solid black; padding: 4px;">
+      <slot />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import Hello from './AnotherComponent.vue'
 
 const count = ref(0)
 </script>
 
 <style scoped>
-
+.slot {
+  border: 1px solid black;
+  padding: 4px;
+}
 </style>
